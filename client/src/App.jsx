aaa62e77a50737bba5d5733cbefba1b0ca331e6d
fraxi0n <f.brias@laposte.net>
2023-01-10 10:59:39 +0100

    card done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import Header from './Header'
-import reactLogo from './assets/react.svg'
+import Header from './components/Header/Header'
+import Card from './components/Card/Card'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -19,8 +20,37 @@ function App() {
 
       <p>Avez vous dis bonjour à:</p>
 
+      liste
+      <i class="fa-solid fa-list"></i>
 
-      {/* <Card/> */}
+      intranet
+      <i class="fa-solid fa-network-wired"></i>
+
+      log in
+      <i class="fa-solid fa-right-to-bracket"></i>
+
+      log out
+      <i class="fa-solid fa-right-from-bracket"></i>
+
+      calendar
+      <i class="fa-regular fa-calendar"></i>
+      <br></br>
+
+
+      {/* <FontAwesomeIcon icon="fa-solid fa-cake-candles" /> */}
+
+
+      <Card
+        props={
+
+          JSON.parse(
+            '{"id": "37","gender": "male","firstname": "Isaac","lastname": "Renard","email": "isaac.renard@example.com","phone": "05-49-13-09-86",  "birthdate": "1991-09-17",      "city": "Angers",      "country": "France",      "photo": "https://randomuser.me/api/portraits/men/84.jpg",      "service": "Marketing"            }'
+          )
+
+        }
+      />
+
+
 
       <button>DIRE BONJOUR A QUELQU'UN D'AUTRE</button>
 
